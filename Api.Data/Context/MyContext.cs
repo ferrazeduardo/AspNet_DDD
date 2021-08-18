d7ceef463b5b4a92aef9eab2fb7fffd51ccd6764
Eduardo Borges Ferraz <ebferraz@aslan.br>
2021-08-17 23:54:03 -0300

    criando o context

---
+++ Api.Data/Context/MyContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> option) : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    }
+}
