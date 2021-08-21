017915f3a301f356a7633beb06c03c42be39ba35
Eduardo Borges Ferraz <ebferraz@aslan.br>
2021-08-21 12:12:25 -0300

    input_async

---
+++ Api.Data/Repository/BaseRepository.cs
@@ -0,0 +1,68 @@
+﻿using Data.Context;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where  T : BaseEntity
+    {
+        protected readonly MyContext _context;
+        private DbSet<T> _dataSet;
+
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataSet = _context.Set<T>();
+        }
+
+        public Task<bool> DeleteAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> InsertAsync(T item)
+        {
+            try
+            {
+
+                if(item.Id == Guid.Empty)
+                {
+                    item.Id = Guid.NewGuid();
+                }
+
+                item.CreateAt = DateTime.Now;
+                _dataSet.Add(item);
+
+                await _context.SaveChangesAsync();
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return item;
+        }
+
+        public Task<T> SelectAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
