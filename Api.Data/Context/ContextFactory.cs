5c6bc8a29612e9b1279ade6397623c2ad0786a50
Eduardo Borges Ferraz <ebferraz@aslan.br>
2021-08-21 00:56:24 -0300

    criar usermap

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=localhost;port=3306;Database=dbApi;Uid=auth_windows;Persist Security Info=True;User=root;Password=123456;";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySQL(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
