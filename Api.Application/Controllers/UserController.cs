64b7a35b5686cead3cd3601e33064ed55ccc1ba7
Eduardo Borges Ferraz <ebferraz@aslan.br>
2021-08-24 01:19:37 -0300

    adicionar class UserController

---
+++ Api.Application/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace applicatioon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromServices] IUserService userService)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+
+            }
+            catch (ArgumentException e)
+            {
+               return StatusCode((int) HttpStatusCode.InternalServerError,e.Message);
+            }
+        }
+    }
+}
